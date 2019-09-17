c1aff684e00fa6a9cd620dc0e41cbe0e162df9dd
Navin Alagesan <51852468+Gavin-salamander@users.noreply.github.com>
2019-09-17 07:55:21 +0530

    Update header.php

---
+++ header.php
@@ -0,0 +1,80 @@
+<?php 
+     session_start(); 
+     
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+   <!-- <meta charset="utf-8"> -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136592143-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-136592143-1');
+</script>
+<link rel="icon" href="images/softtestlogo.png">
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    
+	<meta charset="utf-8">
+<title>LIBRARY Mangement System | Salamander PVT LTD</title>
+
+<meta name="author" content="Salamander PVT LTD" />
+
+	
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
+    <!-- jquery ui -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="js/no.js"></script>
+    <script src="js/requirement.js"></script>
+    <!-- css files -->
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/navbar.css">
+	<link rel="stylesheet" href="css/tutorial.css">
+    
+    <!-- google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Anaheim' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Amiko' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Cuprum' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Kodchasan' rel='stylesheet'>
+
+	<link rel="icon" href="images/Logo.png">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script> -->
+    <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <!-- 
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <script src="assets/js/bootstrap.js"></script> -->
+
+</head>
+<body>
+  
+
+<nav class="navbar navbar-default navbar-fixed-top" id="navbar"> 
+	<div class="container container-fluid" id="no_po">
+
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.php">
+				<img src="images/EPCM.png" alt="error_logo" style="height:50px; width:auto;">
+			</a>
+		</div>		
+		<ul class="nav navbar-nav"  id="menu-all">
+			<li><a href="index.php" id="dropbtn">Home</a></li>
+			<li><a href="Add_new_entries.php" id="dropbtn">Add New Entries</a></li>
+			<li><a href="Check-out_books.php" id="dropbtn">Check-out Books</a></li>
+			<li><a href="Check-in_books.php" id="dropbtn">Check-in Books</a></li>
+			<li><a href="Check-database.php" id="dropbtn">Database Statistics</a></li>
+		</ul>		
+	</div>
+</nav>
