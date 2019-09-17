466324ad606d36e32a93cc7465cf4e194f4ba830
Navin Alagesan <51852468+Gavin-salamander@users.noreply.github.com>
2019-09-17 07:55:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,37 @@
+
 <?php
- if(!empty($_SESSION['sname'])){
-   header('location:login.php');
- }
- else {
-    ?>
-<?php
-include('header_top.php');
-include('index_seo.php');
-include('header.php');
-include('navbar.php');
+
+include('Header.php');
  ?>
 
-
-
-
-<?php 
-include 'contact.php';
-?>
+<SECTION id="formcss">
+	<div class="container lotofgap">
+		<div class="row">		
+			<h1>Welcome To Library Management System</h1>
+                <div class="col-sm-3">
+					<form action="Add_new_entries.php" method="get" >
+						<button type="submit">Adding New Entries</button>
+					</form>
+				</div>
+				<div class="col-sm-3">
+					<form action="Check-out_books.php" method="get" >
+						 <button type="submit">For Cheching Books Out</button>
+					</form>
+				</div>
+				<div class="col-sm-3">
+					<form action="Check-in_books.php" method="get" >
+						 <button type="submit">For Cheching Books In</button>
+					</form>
+				</div>
+				<div class="col-sm-3">
+					<form action="Check-database.php" method="get" >
+						 <button type="submit">Database Statistics</button>
+					</form>
+				</div>
+			</div>
+	</div>
+</SECTION>
 
 <?php
-    include("footer.php");
+    include("Footer.php");
 ?>
-<?php } ?>
