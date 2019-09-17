d676b4c38a74c2c6c5dead7312592c434ec276c2
Navin Alagesan <51852468+Gavin-salamander@users.noreply.github.com>
2019-09-17 07:55:23 +0530

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="row">
+					<div class="col-sm-4" id="quickly">
+						<img alt src="images/Footerlogo.png" height="120px" >
+					</div>	
+					<div class="col-sm-4" id="quickly">
+						<h4>East Point Academy</h4>
+						<b><i class="fas fa-map-marker-alt fa-2x" style="color:#ff8e00;"></i> &nbsp;India</b>
+						<ul class="list-group">
+							<li class="list-group-item">Address</li>
+							<li class="list-group-item">#10, 80 Ft.Rd, HRBR Layout, Kalyan Nagar, Bangalore - 560043</li>
+							<li> Email: <a href="mailto:info@eastpoint.ac.in">info@eastpoint.ac.in</a></li>
+							<li> Email: <a href="mailto:admissions@eastpoint.ac.in">admissions@eastpoint.ac.in</a></li>							
+						</ul>							
+					</div>					
+					<div class="col-sm-4" id="social">
+						<p class="text-center">Social</p>
+						<ul class="list-group text-center">
+							<li class="list-group-item"><a href="https://www.facebook.com/" target="_blank"><span class="fab fa-facebook fa-1x fb"></span></a></li>
+							<li class="list-group-item"><a href="https://twitter.com/" target="_blank"><span class="fab fa-twitter fa-1x tweet"></span></a></li>
+							<li class="list-group-item"><a href="https://www.linkedin.com/" target="_blank"><span class="fab fa-linkedin fa-1x link"></span></a></li>
+							<li class="list-group-item"><a href="https://www.youtube.com/" target="_blank"><span class="fab fa-youtube-square fa-1x youtube"></span></a></li> 							
+						</ul>						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>	
+</body>	
+</html>
